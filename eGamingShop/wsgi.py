da21531160ee5cee64dd19471bc0f4af9a91418d
Shamaun-Nabi <18201050@uap-bd.edu>
2021-02-12 18:04:03 +0600

    Virtual env and Project eGaming shop added with django

---
+++ eGamingShop/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for eGamingShop project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eGamingShop.settings')
+
+application = get_wsgi_application()
