f079b54703bc13332dcade1f517a3820c5958668
Shamaun-Nabi <18201050@uap-bd.edu>
2021-03-20 22:25:04 +0600

    Admin Panel Modified

--- GamingShop/admin.py
+++ GamingShop/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+
+
+#------------------- ADMIN PAGE CUSTOMIZE------------
+admin.site.site_header="Login To WFG Gaming"
+admin.site.site_title="Dashboard"
+admin.site.index_title="Welcome To This Dashboard"
